667b1ac04f3c7b59aff0ecb72409a0c86ca88e03
Kevin Chappell <kevin.b.chappell@gmail.com>
2015-06-05 16:03:56 -0700

    update latest post widget

--- widgets/latest_posts.php
+++ widgets/latest_posts.php
@@ -46,12 +46,12 @@ class Kwik_Latest_Posts_Widget extends WP_Widget {
 	 * How to display the widget on the screen.
 	 */
 	function widget( $args, $instance ) {
-	    $inputs = new KwikInputs;
+		$inputs = new KwikInputs;
 		extract( $args );
 
 		/* Our variables from the widget settings. */
 		$title = apply_filters( 'widget_title', $instance['title'] );
-	    $category_id = intval( $instance['category_id'] );
+		$category_id = intval( $instance['category_id'] );
 		$tag_id = $instance['tag_id'];
 		$num_posts = absint( $instance['num_posts'] );
 		$post_offset =  absint( $instance['post_offset'] );
@@ -68,6 +68,7 @@ class Kwik_Latest_Posts_Widget extends WP_Widget {
 		$date_style =  $instance['date_style'];
 		$show_all_link = isset( $instance['show_all_link'] ) ? $instance['show_all_link'] : false;
 		$show_all_text = $instance['show_all_text'];
+		$show_all_text_position = isset( $instance['show_all_text_position'] ) ? $instance['show_all_text_position'] : 'top';
 		$views_posts_link = '';
 
 		if ( $show_all_link ){
@@ -84,7 +85,12 @@ class Kwik_Latest_Posts_Widget extends WP_Widget {
 
 		/* Display the widget title if one was input (before and after defined by themes). */
 		if ( $title ) {
-			echo $before_title . $title . $views_posts_link . $after_title;
+			echo $before_title;
+			echo $title;
+			if( $show_all_text_position === 'top' ) {
+				echo $views_posts_link;
+			}
+			echo $after_title;
 		}
 
 		/* Display the Latest Items accordingly... */
@@ -142,6 +148,9 @@ class Kwik_Latest_Posts_Widget extends WP_Widget {
 	</div><!-- end widget -->
 	<?php endif; wp_reset_postdata();
 
+		if ( $show_all_text_position === 'bottom' ) {
+			echo $views_posts_link;
+		}
 		/* After widget (defined by themes). */
 		echo $after_widget;
 	}
@@ -163,13 +172,14 @@ class Kwik_Latest_Posts_Widget extends WP_Widget {
 		$instance['date_style'] = strip_tags( $new_instance['date_style'] );
 		$instance['neat'] = $new_instance['neat'];
 
-	    $instance['category_id'] = esc_attr( $new_instance['category_id']);
+		$instance['category_id'] = esc_attr( $new_instance['category_id']);
 		$instance['tag_id'] = esc_attr( $new_instance['tag_id']);
 		$instance['read_more'] = $new_instance['read_more'];
 		$instance['show_thumbs'] = $new_instance['show_thumbs'];
 		$instance['post_thumb_width'] = ( $new_instance['post_thumb_width'] ) ? absint(strip_tags( $new_instance['post_thumb_width'] )) : 60;
 		$instance['post_thumb_height'] = ( $new_instance['post_thumb_height'] ) ? absint(strip_tags( $new_instance['post_thumb_height'] )) : 60;
 		$instance['show_all_text'] = $new_instance['show_all_text'];
+		$instance['show_all_text_position'] = $new_instance['show_all_text_position'];
 		$instance['show_all_link'] = strip_tags( $new_instance['show_all_link']);
 
 		return $instance;
@@ -187,7 +197,7 @@ class Kwik_Latest_Posts_Widget extends WP_Widget {
 		$defaults = array(
 			'title' => esc_html__( 'Latest Items', 'kwik' ),
 			'tag_id' => '',
-		    'category_id' => '',
+			'category_id' => '',
 			'num_posts' => 3,
 			'post_offset' => 0,
 			'post_type' => 'post',
@@ -200,13 +210,14 @@ class Kwik_Latest_Posts_Widget extends WP_Widget {
 			'show_date' => true,
 			'show_all_link' => true,
 			'show_all_text' => 'View all',
+			'show_all_text_position' => 'top',
 			'date_style' => 'human_time_diff',
 			'delim' => '&hellip;',
 			'neat' => true,
 			);
 		$instance = wp_parse_args( (array) $instance, $defaults ); ?>
 
-        <?php
+		<?php
 
 		$post_type = $instance['post_type'];
 		$kc_post_types = get_post_types( array( '_builtin'=> false ) );
@@ -241,7 +252,7 @@ class Kwik_Latest_Posts_Widget extends WP_Widget {
 			<input id="<?php echo $this->get_field_id( 'title' ); ?>" type="text" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" class="widefat" />
 		</p>
 
-    <!-- Widget Title: Post Type -->
+	<!-- Widget Title: Post Type -->
 		<p>
 			<label for="<?php echo $this->get_field_id( 'post_type' ); ?>"><?php esc_html_e( 'Post Type:', 'kwik' ); ?></label>
 			<?php echo $fallback; ?>
@@ -249,12 +260,12 @@ class Kwik_Latest_Posts_Widget extends WP_Widget {
 
 
 		<!-- Show Only Posts with tag -->
-    <p>
-      <label for="<?php echo $this->get_field_id( 'tag_id' ); ?>"><?php esc_html_e( 'Show only posts with this tag(s):', 'kwik' ); ?></label>
-      <input id="<?php echo $this->get_field_id( 'tag_id' ); ?>" type="text" name="<?php echo $this->get_field_name( 'tag_id' ); ?>" value="<?php echo $instance['tag_id']; ?>" class="widefat" />
-    </p>
+	<p>
+	  <label for="<?php echo $this->get_field_id( 'tag_id' ); ?>"><?php esc_html_e( 'Show only posts with this tag(s):', 'kwik' ); ?></label>
+	  <input id="<?php echo $this->get_field_id( 'tag_id' ); ?>" type="text" name="<?php echo $this->get_field_name( 'tag_id' ); ?>" value="<?php echo $instance['tag_id']; ?>" class="widefat" />
+	</p>
 
-    <!-- Show Categories -->
+	<!-- Show Categories -->
 		<p>
 			<label for="<?php echo $this->get_field_id( 'category_id' ); ?>"><?php esc_html_e( '.. in a specific category:', 'kwik' ); ?></label>
 			<?php wp_dropdown_categories( 'show_option_all=All&hierarchical=1&orderby=name&selected='.$instance['category_id'].'&name='.$this->get_field_name( 'category_id' ).'&class=widefat' ); ?>
@@ -281,93 +292,101 @@ class Kwik_Latest_Posts_Widget extends WP_Widget {
 			<label for="<?php echo $this->get_field_id( 'excerpt_length' ); ?>"><?php esc_html_e( 'Excerpt Length:', 'kwik' ); ?></label>
 			<input id="<?php echo $this->get_field_id( 'excerpt_length' ); ?>" type="text" name="<?php echo $this->get_field_name( 'excerpt_length' ); ?>" value="<?php echo $instance['excerpt_length']; ?>" size="2" maxlength="2" />
 		<br/><small><?php esc_html_e( 'Use "0" for no excerpt', 'kwik' ); ?></small>
-        </p>
+		</p>
 
 		<!-- Delimiter -->
 		<p>
 			<label for="<?php echo $this->get_field_id( 'delim' ); ?>"><?php esc_html_e( 'Delimiter:', 'kwik' ); ?></label>
-            <select id="<?php echo $this->get_field_id( 'delim' ); ?>" name="<?php echo $this->get_field_name( 'delim' ); ?>" >
-                <option value="&hellip;" <?php echo ( $instance['delim'] == '…' ? 'selected="selected"' : '' ); ?>>&hellip;</option>
-                <option value="&ndash;" <?php echo ( $instance['delim'] == '–' ? 'selected="selected"' : '' ); ?>>&ndash;</option>
-                <option value="&mdash;" <?php echo ( $instance['delim'] == '—' ? 'selected="selected"' : '' ); ?>>&mdash;</option>
-                <option value="&raquo;" <?php echo ( $instance['delim'] == '»' ? 'selected="selected"' : '' ); ?>>&raquo;</option>
-                <option value="&gt;" <?php echo ( $instance['delim'] == '>' ? 'selected="selected"' : '' ); ?>>&gt;</option>
-            </select>
+			<select id="<?php echo $this->get_field_id( 'delim' ); ?>" name="<?php echo $this->get_field_name( 'delim' ); ?>" >
+				<option value="&hellip;" <?php echo ( $instance['delim'] == '…' ? 'selected="selected"' : '' ); ?>>&hellip;</option>
+				<option value="&ndash;" <?php echo ( $instance['delim'] == '–' ? 'selected="selected"' : '' ); ?>>&ndash;</option>
+				<option value="&mdash;" <?php echo ( $instance['delim'] == '—' ? 'selected="selected"' : '' ); ?>>&mdash;</option>
+				<option value="&raquo;" <?php echo ( $instance['delim'] == '»' ? 'selected="selected"' : '' ); ?>>&raquo;</option>
+				<option value="&gt;" <?php echo ( $instance['delim'] == '>' ? 'selected="selected"' : '' ); ?>>&gt;</option>
+			</select>
 		</p>
 
-    <!-- Show Date -->
+	<!-- Show Date -->
 		<p>
 			<label for="<?php echo $this->get_field_id( 'show_date' ); ?>">
-			    <input class="checkbox" type="checkbox" <?php checked( $instance['show_date'], true ); ?> id="<?php echo $this->get_field_id( 'show_date' ); ?>" class="toggle_field" name="<?php echo $this->get_field_name( 'show_date' ); ?>" value="1" />
-			    <?php esc_html_e( 'Show Date', 'kwik' ); ?>
+				<input class="checkbox" type="checkbox" <?php checked( $instance['show_date'], true ); ?> id="<?php echo $this->get_field_id( 'show_date' ); ?>" class="toggle_field" name="<?php echo $this->get_field_name( 'show_date' ); ?>" value="1" />
+				<?php esc_html_e( 'Show Date', 'kwik' ); ?>
 			</label><br/>
 
 			<label for="<?php echo $this->get_field_id( 'show_date_style' ); ?>" id="<?php echo $this->get_field_id( 'show_date_style' ); ?>-label" style="display: <?php echo $instance['show_date'] ? "inline-block" : "none" ?>">
-			    <?php esc_html_e( 'Date Style', 'kwik' ); ?>
+				<?php esc_html_e( 'Date Style', 'kwik' ); ?>
 
 			<select id="<?php echo $this->get_field_id( 'date_style' ); ?>" name="<?php echo $this->get_field_name( 'date_style' ); ?>" >
-                <option value="human_time_diff" <?php echo ( $instance['date_style'] == "human_time_diff" ? 'selected="selected"' : '' ); ?>><?php echo human_time_diff( current_time( 'timestamp' )-100, current_time( 'timestamp' ) ) . ' ago'; ?></option>
-                <option value="Y-m-d" <?php echo ( $instance['date_style'] == 'Y-m-d' ? 'selected="selected"' : '' ); ?>><?php echo date("Y-m-d") ?></option>
-                <option value="d-m-Y" <?php echo ( $instance['date_style'] == 'd-m-Y' ? 'selected="selected"' : '' ); ?>><?php echo date("d-m-Y") ?></option>
-                <option value="D F dS, Y" <?php echo ( $instance['date_style'] == 'D F dS, Y' ? 'selected="selected"' : '' ); ?>><?php echo date("D F dS, Y") ?></option>
-            </select>
-            </label>
+				<option value="human_time_diff" <?php echo ( $instance['date_style'] == "human_time_diff" ? 'selected="selected"' : '' ); ?>><?php echo human_time_diff( current_time( 'timestamp' )-100, current_time( 'timestamp' ) ) . ' ago'; ?></option>
+				<option value="Y-m-d" <?php echo ( $instance['date_style'] == 'Y-m-d' ? 'selected="selected"' : '' ); ?>><?php echo date("Y-m-d") ?></option>
+				<option value="d-m-Y" <?php echo ( $instance['date_style'] == 'd-m-Y' ? 'selected="selected"' : '' ); ?>><?php echo date("d-m-Y") ?></option>
+				<option value="D F dS, Y" <?php echo ( $instance['date_style'] == 'D F dS, Y' ? 'selected="selected"' : '' ); ?>><?php echo date("D F dS, Y") ?></option>
+			</select>
+			</label>
 
 		</p>
 
 
-    <!-- Show All Link -->
+	<!-- Show All Link -->
 		<p>
 			<label for="<?php echo $this->get_field_id( 'show_all_link' ); ?>">
-			    <input class="checkbox" type="checkbox" <?php checked( $instance['show_all_link'], true ); ?> id="<?php echo $this->get_field_id( 'show_all_link' ); ?>" name="<?php echo $this->get_field_name( 'show_all_link' ); ?>" value="1" />
-			    <?php esc_html_e( 'Show All Link', 'kwik' ); ?>
+				<input class="checkbox" type="checkbox" <?php checked( $instance['show_all_link'], true ); ?> id="<?php echo $this->get_field_id( 'show_all_link' ); ?>" name="<?php echo $this->get_field_name( 'show_all_link' ); ?>" value="1" />
+				<?php esc_html_e( 'Show All Link', 'kwik' ); ?>
 			</label><br/>
 
 			<label for="<?php echo $this->get_field_id( 'show_all_text' ); ?>" id="<?php echo $this->get_field_id( 'show_all_text' ); ?>-label" >
-			    <?php esc_html_e( 'Link Text', 'kwik' ); ?>
+				<?php esc_html_e( 'Link Text', 'kwik' ); ?>
 			<input type="text" id="<?php echo $this->get_field_id( 'show_all_text' ); ?>" value="<?php echo $instance['show_all_text'] ?>"  name="<?php echo $this->get_field_name( 'show_all_text' ); ?>" />
+			</label><br/>
+
+			<label for="<?php echo $this->get_field_id( 'show_all_text_position' ); ?>" id="<?php echo $this->get_field_id( 'show_all_text_position' ); ?>-label" >
+			    <?php esc_html_e( 'Link Text Position', 'kwik' ); ?>
+			<select id="<?php echo $this->get_field_id( 'show_all_text_position' ); ?>" name="<?php echo $this->get_field_name( 'show_all_text_position' ); ?>" >
+				<option value="top" <?php selected( $instance['show_all_text_position'], 'top' ); ?>>Top</option>
+				<option value="bottom" <?php selected( $instance['show_all_text_position'], 'bottom' ); ?>>Bottom</option>
+			</select>
             </label>
 
 		</p>
 
 
-      <!-- Neat Trim -->
+	  <!-- Neat Trim -->
 		<p>
 			<label for="<?php echo $this->get_field_id( 'neat' ); ?>">
-			    <input class="checkbox" type="checkbox" <?php checked( $instance['neat'], true ); ?> id="<?php echo $this->get_field_id( 'neat' ); ?>" name="<?php echo $this->get_field_name( 'neat' ); ?>" value="1" <?php checked( '1', $instance['neat']); ?> />
-			    <?php esc_html_e( 'Neat Trim', 'kwik' ); ?>
+				<input class="checkbox" type="checkbox" <?php checked( $instance['neat'], true ); ?> id="<?php echo $this->get_field_id( 'neat' ); ?>" name="<?php echo $this->get_field_name( 'neat' ); ?>" value="1" <?php checked( '1', $instance['neat']); ?> />
+				<?php esc_html_e( 'Neat Trim', 'kwik' ); ?>
 			</label><br />
-            <small><?php esc_html_e( 'Neat Trim trims on word boundary', 'kwik' ); ?></small>
+			<small><?php esc_html_e( 'Neat Trim trims on word boundary', 'kwik' ); ?></small>
 		</p>
 
 
 		<!-- Show "Read more ->" link checkbox -->
 		<p>
 			<label for="<?php echo $this->get_field_id( 'read_more' ); ?>" style="display:none;">
-			    <input class="checkbox" type="checkbox" <?php checked( $instance['read_more'], true ); ?> id="<?php echo $this->get_field_id( 'read_more' ); ?>" name="<?php echo $this->get_field_name( 'read_more' ); ?>" value="1" <?php checked( '1', $instance['read_more']); ?> />
-			    <?php esc_html_e( 'Show "View" post button', 'kwik' ); ?>
+				<input class="checkbox" type="checkbox" <?php checked( $instance['read_more'], true ); ?> id="<?php echo $this->get_field_id( 'read_more' ); ?>" name="<?php echo $this->get_field_name( 'read_more' ); ?>" value="1" <?php checked( '1', $instance['read_more']); ?> />
+				<?php esc_html_e( 'Show "View" post button', 'kwik' ); ?>
 			</label>
 		</p>
 
 		<!-- Show Thumbnails -->
 		<p>
 			<label for="<?php echo $this->get_field_id( 'show_thumbs' ); ?>">
-			    <input class="checkbox" type="checkbox" <?php checked( $instance['show_thumbs'], true ); ?> id="<?php echo $this->get_field_id( 'show_thumbs' ); ?>" name="<?php echo $this->get_field_name( 'show_thumbs' ); ?>" value="1" <?php checked( '1', $instance['show_thumbs']); ?> />
-			    <?php esc_html_e( 'Show thumbnails', 'kwik' ); ?>
+				<input class="checkbox" type="checkbox" <?php checked( $instance['show_thumbs'], true ); ?> id="<?php echo $this->get_field_id( 'show_thumbs' ); ?>" name="<?php echo $this->get_field_name( 'show_thumbs' ); ?>" value="1" <?php checked( '1', $instance['show_thumbs']); ?> />
+				<?php esc_html_e( 'Show thumbnails', 'kwik' ); ?>
 			</label>
 		</p>
 
 <?php
 if ( $instance['show_thumbs']) : ?>
-    <!-- Thumb Dimension -->
-    <p>
-      <label for="<?php echo $this->get_field_id( 'post_thumb_width' ); ?>"><?php esc_html_e( 'Thumbnail Dimensions:', 'kwik' ); ?></label><br />
-      <input id="<?php echo $this->get_field_id( 'post_thumb_width' ); ?>" type="text" name="<?php echo $this->get_field_name( 'post_thumb_width' ); ?>" value="<?php echo $instance['post_thumb_width']; ?>" size="5" maxlength="4" />
-      <span> &#10005; </span>
-      <input id="<?php echo $this->get_field_id( 'post_thumb_height' ); ?>" type="text" name="<?php echo $this->get_field_name( 'post_thumb_height' ); ?>" value="<?php echo $instance['post_thumb_height']; ?>" size="5" maxlength="4" />
-      <br />
-      <span style="width:58px;display:inline-block;"><?php esc_html_e( 'Width', 'kwik' ); ?></span>&#10005;<span style="margin-left:9px;width:150px;"><?php esc_html_e( 'Height in pixels', 'kwik' ); ?></span>
-    </p>
+	<!-- Thumb Dimension -->
+	<p>
+	  <label for="<?php echo $this->get_field_id( 'post_thumb_width' ); ?>"><?php esc_html_e( 'Thumbnail Dimensions:', 'kwik' ); ?></label><br />
+	  <input id="<?php echo $this->get_field_id( 'post_thumb_width' ); ?>" type="text" name="<?php echo $this->get_field_name( 'post_thumb_width' ); ?>" value="<?php echo $instance['post_thumb_width']; ?>" size="5" maxlength="4" />
+	  <span> &#10005; </span>
+	  <input id="<?php echo $this->get_field_id( 'post_thumb_height' ); ?>" type="text" name="<?php echo $this->get_field_name( 'post_thumb_height' ); ?>" value="<?php echo $instance['post_thumb_height']; ?>" size="5" maxlength="4" />
+	  <br />
+	  <span style="width:58px;display:inline-block;"><?php esc_html_e( 'Width', 'kwik' ); ?></span>&#10005;<span style="margin-left:9px;width:150px;"><?php esc_html_e( 'Height in pixels', 'kwik' ); ?></span>
+	</p>
 
 <?php endif; ?>
 
